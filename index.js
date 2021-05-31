08ad59d4652af4db118235bb43a6bb2cbdad7330
Tommy Myers <tommy@tommymyers.dev>
2021-05-31 12:55:32 +0100

    exercise 3.1

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete("/api/person/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing"
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing"
+    })
+  }
+
+  const person = {
+    ...body,
+    id: generateId()
+  }
+
+  persons.push(person)
+
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on http://127.0.0.1:${PORT}`)
+})

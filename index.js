5e613041c862cd5b37460f7b3b30cc79e7c89c40
Tommy Myers <tommy@tommymyers.dev>
2021-05-31 20:52:07 +0100

    exercise 3.11

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token("body", (request, response) => {
   return request.method === "POST" ? JSON.stringify(request.body) : ""
 })
 
+app.use(express.static("build"))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))

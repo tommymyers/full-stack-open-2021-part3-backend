01e11d493282663b1aa222153a194ce47131fbea
Tommy Myers <tommy@tommymyers.dev>
2021-06-07 19:54:10 +0100

    exercise 3.17

--- index.js
+++ index.js
@@ -62,6 +62,33 @@ app.post("/api/persons", (request, response) => {
   })
 })
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
 app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p>${new Date()}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
+const url = process.env.MONGODB_URI
+console.log("connecting to", url)
 mongoose
   .connect(url, {
     useNewUrlParser: true,
@@ -10,22 +10,23 @@ mongoose
     useCreateIndex: true,
   })
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log("connected to MongoDB")
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log("error connecting to MongoDB:", error.message)
+  })
+
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String
-});
+  number: String,
+})
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model("Person", personSchema)
